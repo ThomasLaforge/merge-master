c13d72db703b134bf174722c2b34e6a5f50871cc
Thomas Laforge <thomas.laforge.38@gmail.com>
2021-01-26 17:42:04 +0100

    better chest generator

--- client/src/modules/UI.ts
+++ client/src/modules/UI.ts
@@ -1,16 +1,16 @@
-import { HeroCollectionElt } from "./definitions"
-
-export interface HeroData {
-    id: number,
-    name: string,
-    color: string
-}
+import { HeroCollectionElt, NB_HERO_TYPE, getAllHeroes } from "./definitions"
+import { shuffle } from "./tools"
 
 export function generateRandomChest(price: number){
-    return [{
-        id: 1,
-        number: 3
-    }] as Chest
+    const nbHeroesType = 3
+    const heroTypes = shuffle(getAllHeroes()).slice(0, nbHeroesType)
+    return heroTypes.map(t => {
+        const number = Math.floor(Math.random() * 20) + 1
+        return {
+            id: t, 
+            number
+        }
+    })
 }
 
 export type Chest = HeroCollectionElt[]
