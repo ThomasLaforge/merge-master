f0b355472ff8bcac387a1d5ca721bae9571af693
Thomas Laforge <thomas.laforge.38@gmail.com>
2021-01-26 14:45:28 +0100

    better get level with more informations

--- client/src/modules/Hero.ts
+++ client/src/modules/Hero.ts
@@ -7,14 +7,27 @@ export default class Hero {
 
     }
 
-    get level(){
+    get stockState(){
         let rest = this.amount
         let level = 0
         while(rest > 0){
             rest -= Math.pow(2, level + 1) 
             level++
         }
-        return level
+        console.log({
+            level,
+            neededForNextLevel: Math.pow(2, level),
+            currentOnLevel: -rest
+        })
+        return {
+            level,
+            neededForNextLevel: Math.pow(2, level + 1),
+            currentOnLevel: -rest
+        }
+    }
+
+    get level(){
+        return this.stockState.level
     }
 
     get currentLevelAmount(){
